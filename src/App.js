641f5b50fe271aee973fa91c2fa968062cd8b5a0
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-04-20 03:06:13 +0900

    using place layouts & data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+import Container from 'react-bootstrap/Container';
+
 import Header from './components/Header.jsx';
+import Place from './layouts/Place.jsx';
+
+import places from './data/places.json';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      places: places
+    }
+  }
+
   render() {
     return (
-      <Header />
+      <div className="app">
+        <Header />
+          <Container className="mt-5">
+            {this.state.places.map((place) => {
+              return <Place place={place}/>;
+            })}
+        </Container>
+      </div>
     );
   };
 }
